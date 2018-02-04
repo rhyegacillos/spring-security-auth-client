53071ac7caf0a34a3049f8434bedc1655e107d1a
Rhye Gacillos <gacillos.rhye@gmail.com>
2018-02-05 05:39:51 +0800

    Spring Security OAuth Client

---
+++ src/main/java/com/spring/security/springsecurityauthclient/SpringSecurityAuthClientApplication.java
@@ -0,0 +1,12 @@
+package com.spring.security.springsecurityauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthClientApplication.class, args);
+	}
+}
